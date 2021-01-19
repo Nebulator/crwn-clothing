c43983eeab8f3ced04623ab8ded35fdd5346fe85
Nebulator <you@example.com>
2021-01-19 08:51:02 -0700

    Added checkout-item quantity arrows. Can change quantity from cart.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,3 +10,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id);
+    if (existingCartItem && existingCartItem.quantity > 1) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+        )
+    }
+    else if (existingCartItem && existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+    else {
+        console.error("Attempted to remove an item from the cart that either doesn't exist or has a quantity of 0")
+    }
+}
